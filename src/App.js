ea5a3b8e0692321e79686560e6d9d05cd7e4de58
soohyunnn <sh.choi@cbcm-co.com>
2022-12-17 18:25:55 +0900

    한 번에 끝내는 React - React 공식문서로 디테일잡기 2-6 LifeCycle

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import "./App.css";
 import Extraction from "./components/2-4.Props/Extraction/Extraction";
 import Composition from "./components/2-4.Props/Composition";
 import ClassComponent from "./components/2-5.State/ClassComponent";
+import ClassComponent2 from "./components/2-6. LifeCycle/ClassComponent";
 import FunctionalComponent from "./components/2-5.State/FunctionalComponent";
 
 function App() {
   return (
     <div className="App">
-      <FunctionalComponent />
+      <ClassComponent2 />
+      {/* <FunctionalComponent />
       <ClassComponent />
       <Extraction />
-      <Composition />
+      <Composition /> */}
     </div>
   );
 }
