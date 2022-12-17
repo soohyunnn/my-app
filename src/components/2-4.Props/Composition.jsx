b180467ced4c924c947484eb24b752bf58d0889b
soohyunnn <sh.choi@cbcm-co.com>
2022-12-17 15:51:46 +0900

    한 번에 끝내는 React - React 공식문서로 디테일잡기 2-4 props

---
+++ src/components/2-4.Props/Composition.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+export default function Composition() {
+  return (
+    <div>
+      <Welcome name="pia" />
+    </div>
+  );
+}
