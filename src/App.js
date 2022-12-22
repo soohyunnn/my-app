2a8b53b9f7ad7674f36daf1d8cf93e2e6525679b
soohyunnn <sh.choi@cbcm-co.com>
2022-12-22 09:25:59 +0900

    한 번에 끝내는 React - React 라이브러리 - emotion

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import DateFnsExample from "./components/Part2/DateFns/DateFnsExample";
 import DayjsExample from "./components/Part2/Dayjs/DayjsExample";
+import EmotionExample from "./components/Part2/Emotion/EmotionExample";
 import MomentExample from "./components/Part2/MomentExample";
 import StyledComponentExample from "./components/Part2/StyledComponent/StyledComponentExample";
 
 function App() {
   return (
     <div className="App">
-      <StyledComponentExample />
+      <EmotionExample />
+      {/* <StyledComponentExample /> */}
       {/* <DateFnsExample /> */}
       {/* <DayjsExample /> */}
       {/* <MomentExample /> */}
